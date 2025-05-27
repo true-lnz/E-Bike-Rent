35674121a960e3da636908553adc8e18c89eaedd
AdWanTay <63309077+AdWanTay@users.noreply.github.com>
2025-05-27 21:52:33 +0500

    маленький фикс

--- internal/models/maintenance.go
+++ internal/models/maintenance.go
@@ -7,7 +7,7 @@ type Maintenance struct {
 	UserID       uint      `gorm:"not null" json:"user_id"`
 	User         User      `gorm:"constraint:OnDelete:CASCADE;" json:"user"`
 	BicycleName  string    `gorm:"not null" json:"bicycle_name"`
-	Status       string    `gorm:"not null default:'заявка в обработке'" json:"status"`
+	Status       string    `gorm:"not null;default:'заявка в обработке'" json:"status"`
 	Details      string    `gorm:"not null" json:"details"`
 	AdminMessage string    `json:"admin_message"`
 	CreatedAt    time.Time `gorm:"not null" json:"created_at"`
