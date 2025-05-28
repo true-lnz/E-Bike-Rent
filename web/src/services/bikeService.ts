785570a016d16c53d22140bda7466acf1219c357
AdWanTay <63309077+AdWanTay@users.noreply.github.com>
2025-05-28 19:27:32 +0500

    поменял константы

--- web/src/services/bikeService.ts
+++ web/src/services/bikeService.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
-import type { Bike } from "../types/bike";
+import type {Bike} from "../types/bike";
+import {BASE_URL} from "../constants.ts";
 
 // Получение всех велосипедов
 export const getAllBikes = async (): Promise<Bike[]> => {
-  const response = await axios.get<{ items: Bike[] }>("http://127.0.0.1:8080/api/bicycle");
-  return response.data.items;
+    const response = await axios.get<{ items: Bike[] }>(BASE_URL + "api/bicycle");
+    return response.data.items;
 };
 
 // Получение одного велосипеда по ID
 export const getBikeById = async (id: number): Promise<Bike> => {
-  const response = await axios.get<Bike>(`http://127.0.0.1:8080/api/bicycle/${id}`);
-  return response.data;
+    const response = await axios.get<Bike>(BASE_URL + `api/bicycle/${id}`);
+    return response.data;
 };
