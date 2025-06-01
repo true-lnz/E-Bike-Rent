55423963bf440563ad2435c168a37691f7df2cde
true-lnz <lansonzgroup@yandex.ru>
2025-06-01 22:28:17 +0500

    Исправил баг деплоя

--- web/src/pages/AdminPage.tsx
+++ web/src/pages/AdminPage.tsx
@@ -1,5 +1,6 @@
 // src/pages/AdminPage.jsx
-import BikeList from "../components/BikeList";
+
+import BikeList from "../components/Bikes/BikeList";
 
 export default function AdminPage() {
 	return (
